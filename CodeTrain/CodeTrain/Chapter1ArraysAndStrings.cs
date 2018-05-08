adbc3534cab0e09d4a991bcbc3b3f9b08374a024
vitaliy.efimenko <vitaliy.efimenko@filuet.ru>
2018-05-08 17:00:36 +0300

    Chapter1 from 1.1 to 1.4

---
+++ CodeTrain/CodeTrain/Chapter1ArraysAndStrings.cs
@@ -0,0 +1,128 @@
+﻿using System.Text;
+
+namespace CodeTrain
+{
+    public class Chapter1ArraysAndStrings
+    {
+        public bool CharsCounter_AllCharsUnique(string input)
+        {
+            var arr = new int[char.MaxValue];
+            foreach (var ch in input)
+            {
+                arr[ch]++;
+                if (arr[ch] > 1)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool CharsCounter_AllCharsUnique2(string input)
+        {
+            var str = Sort(input.ToCharArray());
+
+            char tmp = '\0';
+            foreach (var ch in str)
+            {
+                if (ch == tmp)
+                    return false;
+                tmp = ch;
+            }
+
+            return true;
+        }
+
+        public char[] Sort(string arr)
+        {
+            return Sort(arr.ToCharArray());
+        }
+
+        public char[] Sort(char[] arr)
+        {
+            var tmp = new char[arr.Length];
+            arr.CopyTo(tmp,0);
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                for (int j = tmp.Length - 1; j >= 0; j--)
+                {
+                    if (i < j && tmp[i] > tmp[j])
+                    {
+                        char c = tmp[i];
+                        tmp[i] = tmp[j];
+                        tmp[j] = c;
+                    }
+                }
+            }
+
+            return tmp;
+        }
+
+        public bool CheckReversalStrings(string str1, string str2)
+        {
+            if (string.IsNullOrEmpty(str1) || string.IsNullOrEmpty(str2))
+                return false;
+            if (str1.Length != str2.Length)
+                return false;
+
+            var j = str2.Length - 1;
+            for (int i = 0; i < str1.Length; i++, j--)
+            {
+                if (str1[i] != str2[j])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public string ReplaceBlanks(string str, char replace)
+        {
+            var sb = new StringBuilder();
+            bool r = false;
+            foreach (char c in str)
+            {
+                if (c.Equals(' '))
+                {
+                    r = true;
+                    continue;
+                }
+                if(r)
+                {
+                    sb.Append(replace);
+                    r = false;
+                }
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        public bool CheckPalindrome(string str1, string str2)
+        {
+            if (string.IsNullOrEmpty(str1) || string.IsNullOrEmpty(str2))
+                return false;
+            if (str1.Length != str2.Length)
+                return false;
+            var tmp = str2.ToCharArray();
+
+            foreach (var ch in str1)
+            {
+                var find = false;
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    if (ch == tmp[i])
+                    {
+                        tmp[i] = (char) 0;
+                        find = true;
+                        break;
+                    }
+                }
+
+                if (!find)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CodeTrainUnitTest/CodeTrainUnitTest/UnitTest_Chapter1.cs
@@ -0,0 +1,61 @@
+using System;
+using CodeTrain;
+using Xunit;
+
+namespace CodeTrainUnitTest
+{
+    public class UnitTest_Chapter1
+    {
+        [Fact]
+        public void Test_CharsCounter_AllCharsUnique()
+        {
+            var ch1 = new Chapter1ArraysAndStrings();
+            Assert.Equal(true, ch1.CharsCounter_AllCharsUnique("aAbBcCdD"));
+            Assert.Equal(false, ch1.CharsCounter_AllCharsUnique("aAbBcCdDaAbBcCdD"));
+        }
+
+        [Fact]
+        public void Test_CharsCounter_AllCharsUnique2()
+        {
+            var ch1 = new Chapter1ArraysAndStrings();
+            Assert.Equal(true, ch1.CharsCounter_AllCharsUnique2("aAbBcCdD"));
+            Assert.Equal(false, ch1.CharsCounter_AllCharsUnique2("aAbBcCdDaAbBcCdD"));
+        }
+
+        [Fact]
+        public void Test_CharsCounter_Sort()
+        {
+            var ch1 = new Chapter1ArraysAndStrings();
+            Assert.Equal("abcd".ToCharArray(), ch1.Sort("bcda"));
+            Assert.Equal("ABCDabcd".ToCharArray(), ch1.Sort("aBcDAbCd"));
+        }
+
+        [Fact]
+        public void Test_CheckReversalStrings()
+        {
+            var ch1 = new Chapter1ArraysAndStrings();
+            Assert.Equal(true, ch1.CheckReversalStrings("abcde","edcba"));
+            Assert.Equal(false, ch1.CheckReversalStrings("abcde","edcab"));
+        }
+
+        [Fact]
+        public void Test_ReplaceBlanks()
+        {
+            var ch1 = new Chapter1ArraysAndStrings();
+            Assert.Equal("Mr\rJohn\rSmith", ch1.ReplaceBlanks("Mr John   Smith   ", (char) 13));
+        }
+
+        [Fact]
+        public void Test_CheckPalindrome()
+        {
+            var ch1 = new Chapter1ArraysAndStrings();
+            Assert.Equal(true, ch1.CheckPalindrome("tact coa", "tact coa"));
+            Assert.Equal(true, ch1.CheckPalindrome("tact coa","taco cat"));
+            Assert.Equal(true, ch1.CheckPalindrome("tact coa", "atco cta"));
+            Assert.Equal(false, ch1.CheckPalindrome("tact coa", "atco ctc"));
+            Assert.Equal(false, ch1.CheckPalindrome("tact coa", "tactcoa"));
+        }
+
+
+    }
+}
