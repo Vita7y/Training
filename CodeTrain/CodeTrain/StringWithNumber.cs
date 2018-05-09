ecce03df035d73ab69d8e56457215b54b29dc564
vitallium <vitallium@yandex.ru>
2018-05-09 10:44:32 +0300

    Add StringNumberIncrace

---
+++ CodeTrain/CodeTrain/StringWithNumber.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeTrain
+{
+    public class StringWithNumber
+    {
+        public static string Incrace(string str)
+        {
+            var s = "";
+            return Incrace(str, ref s);
+        }
+
+        private static string Incrace(string str, ref string num)
+        {
+            if (string.IsNullOrEmpty(str) || !IsNumber(str.Last()))
+                return str + IncraseNumString(num);
+
+            num += str.Last();
+            return Incrace(str.Substring(0, str.Length - 1), ref num);
+        }
+
+        private static string IncraseNumString(string num)
+        {
+            var resNum = num.ToCharArray();
+
+            for (var i = 0; i < resNum.Length; i++)
+            {
+                var ii = int.Parse(resNum[i].ToString());
+                ii++;
+                if (ii > 9)
+                {
+                    resNum[i] = '0';
+                }
+                else
+                {
+                    resNum[i] = Convert.ToChar(ii.ToString());
+                    break;
+                }
+            }
+
+            return new string(resNum.Reverse().ToArray());
+        }
+
+        public static bool IsNumber(char ch)
+        {
+            switch (ch)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ CodeTrainUnitTest/CodeTrainUnitTest/UnitTest_StringWithNumber.cs
@@ -0,0 +1,44 @@
+﻿using CodeTrain;
+using Xunit;
+
+namespace CodeTrainUnitTest
+{
+    public class UnitTest_StringWithNumber
+    {
+        [Fact]
+        public void Test_Incrace_000()
+        {
+            var res = StringWithNumber.Incrace("000");
+            Assert.Equal("001", res);
+        }
+
+        [Fact]
+        public void Test_Incrace_999()
+        {
+            var res = StringWithNumber.Incrace("999");
+            Assert.Equal("000", res);
+        }
+
+        [Fact]
+        public void Test_Incrace_A0B001()
+        {
+            var res = StringWithNumber.Incrace("A0B001");
+            Assert.Equal("A0B002", res);
+        }
+
+        [Fact]
+        public void Test_Incrace_A0B0129()
+        {
+            var res = StringWithNumber.Incrace("A0B0129");
+            Assert.Equal("A0B0130", res);
+        }
+
+        [Fact]
+        public void Test_Incrace_A0B999()
+        {
+            var res = StringWithNumber.Incrace("A0B999");
+            Assert.Equal("A0B000", res);
+        }
+
+    }
+}
