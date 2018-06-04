4f4cea4288a934f05716a1ae9769072a17419be0
vita7y <vitaliy.efimenko@filuet.ru>
2018-06-04 18:03:09 +0300

    Add Chapter4
    Ex 4.1

---
+++ CodeTrain/CodeTrain/Chapter4Trees.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CodeTrain.DataAlg;
+
+namespace CodeTrain
+{
+    public class Chapter4Trees
+    {
+        public class Graph<T>
+        {
+            public class Node<T>
+            {
+                public Node(T value) : this()
+                {
+                    Value = value;
+                }
+                public Node()
+                {
+                    Nodes = new List<Node<T>>();
+                }
+
+                public T Value { get; set; }
+
+                public List<Node<T>> Nodes { get; set; }
+            }
+
+            public Graph()
+            {
+                Points = new List<Node<T>>();
+            }
+
+            public List<Node<T>> Points { get; set; }
+
+            public void Add(T from, T to)
+            {
+                var nFrom = Points.FirstOrDefault(am => am.Value.Equals(from));
+                if (nFrom == null)
+                {
+                    nFrom = new Node<T>(from);
+                    Points.Add(nFrom);
+                }
+
+                var nTo = Points.FirstOrDefault(am => am.Value.Equals(to));
+                if (nTo == null)
+                {
+                    nTo = new Node<T>(to);
+                    Points.Add(nTo);
+                }
+
+                nFrom.Nodes.Add(nTo);
+            }
+
+            public List<(T from, T to)> FindRoute(T from, T to)
+            {
+                var nFrom = Points.FirstOrDefault(am => am.Value.Equals(from));
+                var nTo = Points.FirstOrDefault(am => am.Value.Equals(to));
+                if (nFrom == null || nTo == null)
+                    return null;
+                return Traverse(nFrom, nTo);
+
+            }
+
+            private List<(T from, T to)> Traverse(Node<T> from, Node<T> to)
+            {
+                foreach (var node in from.Nodes)
+                {
+                    if(node.Value.Equals(to.Value))
+                        return new List<(T from, T to)>{(from.Value, node.Value)};
+                    var res = Traverse(node, to);
+                    if (res == null) continue;
+                    res.Add((from.Value, node.Value));
+                    return res;
+                }
+                return null;
+            }
+        }
+
+        ///4.1. Для заданного направленного графа разработайте алгоритм, проверяющий существование маршрута между двумя узлами.
+        public static List<(string from, string to)> FindRouteBetweenTwoPoints(List<(string p1, string p2)> pointsList, string from, string to)
+        {
+            var graph = new Graph<string>();
+            foreach (var vector in pointsList)
+            {
+                graph.Add(vector.p1, vector.p2);
+            }
+
+            return graph.FindRoute(from, to);
+        }
+    }
+}
---
+++ CodeTrainUnitTest/CodeTrainUnitTest/UnitTest_Chapter4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeTrain;
+using CodeTrain.DataAlg;
+using Xunit;
+
+namespace CodeTrainUnitTest
+{
+    public class UnitTest_Chapter4
+    {
+        [Fact]
+        public void Test_Min()
+        {
+            var res = Chapter4Trees.FindRouteBetweenTwoPoints(new List<(string from, string to)>()
+            {
+                ("Мельбурн", "Кельн"),
+                ("Москва", "Париж"),
+                ("Кельн", "Москва")
+            }, "Мельбурн", "Париж");
+
+            Assert.NotNull(res);
+            Assert.NotNull(res);
+            Assert.Equal(3, res.Count);
+            Assert.Equal(("Мельбурн", "Кельн"), res[2]);
+            Assert.Equal(("Кельн", "Москва"), res[1]);
+            Assert.Equal(("Москва", "Париж"), res[0]);
+        }
+    }
+}
