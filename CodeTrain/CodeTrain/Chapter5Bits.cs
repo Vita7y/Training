9b234a65d872344821605726ddb9f18b83b332d8
vita7y <vitaliy.efimenko@filuet.ru>
2018-06-05 16:37:50 +0300

    Add Chapter5Bits
    Ex. 5.1

---
+++ CodeTrain/CodeTrain/Chapter5Bits.cs
@@ -0,0 +1,47 @@
+﻿namespace CodeTrain
+{
+    public class Chapter5Bits
+    {
+
+        /// <summary>
+        /// 5.1. Даны два 32-разрядных числа N и М и  две позиции битов i и j.
+        ///Напишите метод для вставки м в  N так, чтобы число М занимало позицию с бита j по бит i.
+        ///Предполагается, что j и i имеют такие значения, что число М гарантированно поместится в этот промежуток.
+        ///Скажем, для М = 10011 можно считать, что j и i разделены как минимум 5 битами.
+        ///Комбинация вида j = з и i = 2 невозможна, так как число М не поместится между битом 3 и битом 2.
+        ///Пример:
+        ///Ввод: N = 10000000000, М = 10011, i = 2, j = 6
+        ///Вывод: N = 10001001100
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <param name="res"></param>
+        /// <returns></returns>
+        public static bool InsertBitsIntoNumber(int n, int m, int i, int j, out int res)
+        {
+            res = n;
+
+            int lastBitNum = 0;
+            int tmp = m;
+            for (int ii = 0; ii < 32; ii++)
+            {
+                if ((tmp & 1) == 1)
+                    lastBitNum = ii;
+                tmp = tmp >> 1;
+            }
+
+            if (lastBitNum > (j - i))
+                return false;
+
+            res = res >> j;
+            res = res << j - i;
+            res ^= m;
+            res = res << i;
+            res ^= n;
+
+            return true;
+        }
+    }
+}
---
+++ CodeTrainUnitTest/CodeTrainUnitTest/UnitTest_Chapter5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeTrain;
+using CodeTrain.DataAlg;
+using Xunit;
+
+namespace CodeTrainUnitTest
+{
+    public class UnitTest_Chapter5
+    {
+        [Fact]
+        public void Test_InsertBitsIntoNumber()
+        {
+            int res;
+            var r = Chapter5Bits.InsertBitsIntoNumber(1024, 19, 2, 6, out res);
+            Assert.True(r);
+            Assert.Equal(1100, res);
+            r = Chapter5Bits.InsertBitsIntoNumber(257, 5, 4, 6, out res);
+            Assert.True(r);
+            Assert.Equal(337, res);
+        }
+    }
+}
